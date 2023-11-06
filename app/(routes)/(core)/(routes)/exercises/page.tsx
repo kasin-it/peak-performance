c2b0c589f0fd87c3165442df3e5ba458bcaf293b
KACPER SINILO <kacperosinilo@gmail.com>
2023-11-06 20:18:16 +0100

    fix: query with undefind

--- app/(routes)/(core)/(routes)/exercises/page.tsx
+++ app/(routes)/(core)/(routes)/exercises/page.tsx
@@ -4,37 +4,131 @@ import { useEffect, useState } from "react"
 import axios from "axios"
 
 import { Exercise } from "@/types/types"
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
 
 function ExercisesPage() {
-    const [exercises, setExercises] = useState<null | Exercise[] | string>(null)
+    const [exercises, setExercises] = useState<Exercise[] | null>(null)
     const [error, setError] = useState<string | null>(null)
 
     useEffect(() => {
         const fetchExercises = async () => {
-            const searchParams = new URLSearchParams(window.location.search)
+            try {
+                const searchParams = new URLSearchParams(window.location.search)
 
-            const skill_level = searchParams.get("skill_level")
-            const exercise_type = searchParams.get("exercise_type")
+                const skillLevel = searchParams.get("skill_level")
+                const exerciseType = searchParams.get("exercise_type")
 
-            const response = await axios.get(
-                `/api/exercises?skill_level=${skill_level}&exercise_type=${exercise_type}`
-            )
+                if (!skillLevel || !exerciseType) {
+                    setError("Skill level and exercise type are required.")
+                    return
+                }
 
-            if (response.status !== 200) {
-                setError("Something went wrong.")
-            }
+                const response = await axios.get("/api/exercises", {
+                    params: {
+                        skill_level: skillLevel,
+                        exercise_type: exerciseType,
+                    },
+                })
 
-            if (!response.data) {
-                setExercises("Not found")
-            } else {
-                setExercises(response.data)
+                if (response.status === 200) {
+                    setExercises(response.data)
+                } else {
+                    setError("Something went wrong.")
+                }
+            } catch (error) {
+                setError("An error occurred while fetching data.")
             }
         }
 
         fetchExercises()
     }, [])
 
-    return <div className="h-full pt-96">{JSON.stringify(exercises)}</div>
+    return (
+        <div className="flex flex-col items-center pb-12 pt-48">
+            <h1 className="pb-10 text-6xl font-bold text-black/80">
+                Exercises:
+            </h1>
+            <section className="flex w-full max-w-[1500px] flex-wrap justify-center">
+                {error && <div>{error}</div>}
+                {exercises && exercises.length === 0 && (
+                    <div>No exercises found.</div>
+                )}
+                {exercises && exercises.length > 0 && (
+                    <>
+                        {exercises.map((exercise, index) => (
+                            <Card
+                                className="m-3 mx-auto max-w-md overflow-hidden rounded-xl bg-white shadow-md md:max-w-2xl"
+                                key={index}
+                            >
+                                <div className="p-8">
+                                    <CardTitle className="mt-1 block text-4xl font-medium leading-tight text-black">
+                                        {exercise.name}
+                                    </CardTitle>
+                                    {/* <CardDescription className="mt-2 text-gray-500">
+                                        Start your day with a refreshing morning
+                                        workout.
+                                    </CardDescription> */}
+                                    <div className="mt-4">
+                                        <p className="text-sm text-gray-700">
+                                            Muscles targeted:{" "}
+                                            <span className="font-semibold">
+                                                {exercise.muscle}
+                                            </span>
+                                        </p>
+                                        <p className="mt-2 text-sm text-gray-700">
+                                            Equipment needed:{" "}
+                                            <span className="font-semibold">
+                                                {exercise.equipment}
+                                            </span>
+                                        </p>
+                                        <p className="mt-2 text-sm text-gray-700">
+                                            Type:{" "}
+                                            <span className="font-semibold">
+                                                {exercise.type}
+                                            </span>
+                                        </p>
+                                        <p className="mt-2 text-sm text-gray-700">
+                                            Difficulty:{" "}
+                                            <span className="font-semibold">
+                                                {exercise.difficulty}
+                                            </span>
+                                        </p>
+                                    </div>
+                                    <div className="mt-4">
+                                        <CardTitle className="mt-1 block text-lg font-medium leading-tight text-black">
+                                            Exercise Instructions
+                                        </CardTitle>
+                                        <CardDescription className="mt-2 text-gray-500">
+                                            Follow these instructions to perform
+                                            the exercise correctly:
+                                        </CardDescription>
+
+                                        <p>{exercise.instructions}</p>
+                                    </div>
+                                    <div className="mt-4">
+                                        <Button
+                                            className="text-blue-600 hover:text-blue-900"
+                                            variant="outline"
+                                        >
+                                            Add to my workout
+                                        </Button>
+                                    </div>
+                                </div>
+                            </Card>
+                        ))}
+                    </>
+                )}
+            </section>
+        </div>
+    )
 }
 
 export default ExercisesPage
--- app/api/exercises/route.ts
+++ app/api/exercises/route.ts
@@ -2,11 +2,36 @@ import { NextResponse } from "next/server"
 import axios from "axios"
 
 const BASE_URL = "https://api.api-ninjas.com/v1/exercises"
+
+interface QueryParams {
+    difficulty?: string
+    type?: string
+    muscle?: string
+}
+
 export const GET = async (req: Request) => {
     try {
         const { searchParams } = new URL(req.url)
         const difficulty = searchParams.get("skill_level")
         const type = searchParams.get("exercise_type")
+        const muscle = searchParams.get("muscle")
+
+        const queryParams: QueryParams = {}
+
+        // Add parameters to the queryParams object only if they have values
+        if (difficulty !== null) queryParams.difficulty = difficulty
+        if (type !== null) queryParams.type = type
+        if (muscle !== null) queryParams.muscle = muscle
+
+        // Build the query string from the queryParams object
+        const queryString = Object.keys(queryParams)
+            .map((key) => `${key}=${queryParams[key as keyof QueryParams]}`)
+            .join("&")
+
+        console.log(queryString)
+
+        // Create the complete URL with the query string
+        const searchParamsUrl = queryString ? `?${queryString}` : ""
 
         const config = {
             headers: {
@@ -15,10 +40,7 @@ export const GET = async (req: Request) => {
         }
 
         try {
-            const response = await axios.get(
-                BASE_URL + `?difficulty=${difficulty}&type=${type}`,
-                config
-            )
+            const response = await axios.get(BASE_URL + searchParamsUrl, config)
             const exercises = response.data
             return NextResponse.json(exercises)
         } catch (error) {
