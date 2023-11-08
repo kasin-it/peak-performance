4e3df1e48f2588ef0a2b3fb29714ca3a093ea5d0
KACPER SINILO <kacperosinilo@gmail.com>
2023-11-08 20:09:22 +0100

    feat: add sort

--- app/(routes)/(core)/(routes)/articles/page.tsx
+++ app/(routes)/(core)/(routes)/articles/page.tsx
@@ -1,4 +1,18 @@
-function articles() {
+import { createClient } from "contentful"
+
+async function articles() {
+    const accessToken = process.env.CONTENTFUL_ACCESS_KEY
+    const space = process.env.CONTENTFUL_SPACE_ID
+
+    const contentfulClient = createClient({
+        accessToken: accessToken ? accessToken : "",
+        space: space ? space : "",
+    })
+
+    const res = await contentfulClient.getEntries({
+        content_type: "article",
+    })
+
     return <div>articles</div>
 }
 export default articles
